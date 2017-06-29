7fd9b09a5d26e2f6764d90046132721c46dc9f01
Alex S. Rosa <alexsros@gmail.com>
2017-06-28 23:08:32 -0300

    make it better

--- src/main/java/com/tasima/appmageis/controllers/SampleController.java
+++ src/main/java/com/tasima/appmageis/controllers/SampleController.java
@@ -1,16 +1,15 @@
 package com.tasima.appmageis.controllers;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class SampleController {
 
     @RequestMapping("/")
-    @ResponseBody
     String home() {
         return "index";
     }
