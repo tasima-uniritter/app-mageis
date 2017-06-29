a7687f02477f01c39548f3099c3a3d0bb26de858
Alex S. Rosa <alexsros@gmail.com>
2017-06-28 22:19:58 -0300

    make it better

---
+++ src/main/java/com/tasima/appmageis/AppMageisApplication.java
@@ -0,0 +1,12 @@
+package com.tasima.appmageis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMageisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMageisApplication.class, args);
+	}
+}
