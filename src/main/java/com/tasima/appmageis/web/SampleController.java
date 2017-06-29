e391ddde8b30b486d97e42d6e06871a5f29c4c19
Alex S. Rosa <alexsros@gmail.com>
2017-06-28 23:50:32 -0300

    make it better

--- src/main/java/com/tasima/appmageis/web/SampleController.java
+++ src/main/java/com/tasima/appmageis/web/SampleController.java
@@ -2,10 +2,10 @@ package com.tasima.appmageis.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class SampleController {
 
